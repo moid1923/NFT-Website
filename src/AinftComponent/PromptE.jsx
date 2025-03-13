4180ca1fc80971ff0bd9ad9f76c602c6287a67fb
moid1923 <mueed.octaloop@gmail.com>
2025-03-13 14:46:57 +0500

    nft

--- src/AinftComponent/PromptE.jsx
+++ src/AinftComponent/PromptE.jsx
@@ -54,14 +54,14 @@ const promptData = [
 function PromptE() {
   return (
     <div className="max-w-[1320px] w-full  mx-auto py-10">
-      <h1 className="text-[#2B2B2B] text-center md:text-left font-[Apex_Mk2] text-[64px] font-normal leading-normal tracking-[2.56px] capitalize">
+      <h1 className="text-[#2B2B2B]  text-center md:text-left font-[Apex_Mk2] text-[64px] font-normal leading-normal tracking-[2.56px] capitalize">
         PROMPT EXAMPLES
       </h1>
-      <div className="max-w-[1320px] w-full mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10">
+      <div className="max-w-[1320px] w-full mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-9 px-15my-8 lg:my-15">
         {promptData.map((prompt, index) => (
           <div
             key={index}
-            className="max-w-[577px] lg:h-[262px] w-full h-auto overflow-hidden rounded-2xl shadow-lg p-[15px] bg-white flex flex-col lg:flex-row mx-auto gap-6 "
+            className="max-w-[577px] lg:h-[262px] w-full h-auto overflow-hidden rounded-2xl shadow-lg p-[15px] bg-white flex flex-col lg:flex-row mx-auto gap-6 border-[2.8px] border-solid border-[#E7E7E7]"
           >
             <div className="w-full lg:w-[310px] h-[232px] relative">
               <img
@@ -70,11 +70,11 @@ function PromptE() {
                 className=" w-full h-full  rounded-[20px] object-cover border-3 border-[#E7E7E7]  "
               />
 
-              <button className="w-[58px] h-[58px] rounded-full absolute left-1/2 -bottom-6 lg:top-1/2 lg:left-[100%]  bg-red-500 text-white transform -translate-x-1/2 lg:-translate-y-1/2 flex justify-center items-center border-3 border-[#FFF9F9]">
+              <button className="w-[58px] h-[58px] rounded-full absolute left-1/2 -bottom-6 lg:top-1/2 lg:left-[100%]  bg-gradient-custom text-white transform -translate-x-1/2 lg:-translate-y-1/2 flex justify-center items-center border-3 border-[#FFF9F9]">
                 <span className="text-white font-[Roboto] text-[12px] font-normal leading-normal tracking-[0.36px] opacity-75">
                   Copy
                 </span>
-                <img src={copy} alt="" className="w-[13px] h-[13px] ml-1" />
+                <img src={copy} alt="" className="w-[13.5px] h-[15px] ml-[2px] " />
               </button>
             </div>
 
