0718c86ebee1165962901ed76d60848b9abed072
moid1923 <mueed.octaloop@gmail.com>
2025-03-14 15:03:09 +0500

    nft

--- src/AinftComponent/SellMethod.jsx
+++ src/AinftComponent/SellMethod.jsx
@@ -20,25 +20,25 @@ function SellMethod({ showtick = true, selectedMethod = "Fixed Price" }) {
     navigate("/aipric")
    }
   return (
-    <div className="max-w-[1320px] mx-auto p-2 md:p-6 h-auto min-h-screen overflow-y-auto">
+    <div className="max-w-[1320px] mx-auto p-2 md:p-0 h-auto min-h-screen overflow-y-auto my-20">
       {/* Back Button */}
-      <button className="flex items-center justify-center gap-2 mb- w-[126px] h-16 bg-white rounded-[20px] shadow-[4px_4px_15px_0px_rgba(0,0,0,0.20)] ms-6">
+      <button className="flex items-center justify-center gap-2 my-[30px] w-[126px] h-16 bg-white rounded-[20px] shadow-[4px_4px_15px_0px_rgba(0,0,0,0.20)] ms-6">
         <img src={backicon} alt="Back" className="w-6 h-6" />
         <span className="text-[#2b2b2b] text-xl font-normal font-roboto capitalize tracking-wide">Back</span>
       </button>
 
       {/* Container */}
-      <div className="flex flex-col lg:flex-row gap-[30px] max-w-[1320px] w-full rounded-lg p-6">
+      <div className="flex flex-col lg:flex-row gap-[30px] max-w-[1320px] w-full rounded-lg py-6">
         {/* Preview Section */}
         <div className="w-full md:w-[296px] mx-auto">
-          <div className="flex flex-col gap-4 items-center w-full md:w-[296px] min-h-[309px] bg-white rounded-[20px] shadow-[4px_4px_15px_0px_rgba(0,0,0,0.20)] p-5">
+          <div className="flex flex-col gap-4 items-center max-w-[400px] w-full mx-auto md:w-[296px] min-h-[309px] bg-white rounded-[20px] shadow-[4px_4px_15px_0px_rgba(0,0,0,0.20)] p-5">
             <img
               src={sideimg}
               alt="Preview Your Item"
               className="w-full h-auto rounded-lg shadow-lg"
             />
           </div>
-          <p className="text-[#2b2b2b] text-[22px] font-medium font-roboto capitalize tracking-wide text-center my-[25px]">Preview Your Item</p>
+          <p className="font-roboto font-medium text-[22px] leading-[100%] tracking-[4%] capitalize ]e text-left my-[25px]">Preview Your Item</p>
         </div>
 
         {/* Form Section */}
@@ -51,7 +51,7 @@ function SellMethod({ showtick = true, selectedMethod = "Fixed Price" }) {
               className={`flex flex-col items-center gap-3 justify-center p-4 w-full sm:w-[207.87px] h-[217px] rounded-[14.05px] shadow-[2.8090615272521973px_2.8090615272521973px_10.533980369567871px_0px_rgba(0,0,0,0.20)] relative ${selectedMethod === "Fixed Price" ? "bg-[#f4f3f3]" : "bg-white"}`}
               onClick={handlePrice}
             >
-              <div className="bg-[#45adeb] w-[99px] h-[99px] rounded-full flex items-center justify-center">
+              <div className="bg-[#45adeb] w-[99px] border-[13px] border-solid border-white h-[99px] rounded-full flex items-center justify-center">
                 <img
                   src={fixprice}
                   alt="Fixed Price"
@@ -68,7 +68,7 @@ function SellMethod({ showtick = true, selectedMethod = "Fixed Price" }) {
               className={`flex flex-col items-center gap-3 justify-center p-4 w-full sm:w-[207.87px] h-[217px] rounded-[14.05px] shadow-[2.8090615272521973px_2.8090615272521973px_10.533980369567871px_0px_rgba(0,0,0,0.20)] relative ${selectedMethod === "Auction" ? "bg-[#f4f3f3]" : "bg-white"}`}
               onClick={handleAuc}
             >
-              <div className="bg-[#f7a940] w-[99px] h-[99px] rounded-full flex items-center justify-center">
+              <div className="bg-[#f7a940]   border-[13px] border-solid border-[#F4F4F4]  w-[99px] h-[99px] rounded-full flex items-center justify-center">
                 <img
                   src={auc}
                   alt="Auction"
@@ -84,7 +84,7 @@ function SellMethod({ showtick = true, selectedMethod = "Fixed Price" }) {
 
           {/* Price Section */}
           <div className="mb-6">
-            <label className="block my-[15px]">Price</label>
+            <label className="block my-[15px] text-[#2B2B2B] font-roboto text-[20px] font-medium leading-normal tracking-[0.8px] capitalize">Price</label>
             <div className="flex flex-col md:flex-row rounded-lg overflow-hidden gap-4">
               <div className="flex items-center justify-center gap-3 w-full md:w-[126px] min-h-[58px] bg-white rounded-[15px] border-2 border-[#e7e7e7]">
                 <span className="w-6 h-6 rounded-full bg-black flex items-center justify-center">
@@ -101,7 +101,7 @@ function SellMethod({ showtick = true, selectedMethod = "Fixed Price" }) {
           </div>
           {/*Schedule */}
           <div className="mb-6">
-            <label className="block mb-2">Schedule Listing</label>
+            <label className="block mb-[15px] font-roboto font-medium text-[20px] leading-[100%] tracking-[4%] capitalize">Schedule Listing</label>
             <div className="max-w-[817px] w-full h-[58px] bg-white rounded-[15px] border-2 border-[#e7e7e7] flex  items-center px-[30px] py-[17px]">
               <img src={inp} alt="" className="w-6 h-6" />
               <input
@@ -112,8 +112,8 @@ function SellMethod({ showtick = true, selectedMethod = "Fixed Price" }) {
             </div>
           </div>
           {/* Submit Button */}
-          <div className="w-full flex justify-end">
-            <button className="px-8 py-4 bg-gradient-custom rounded-lg text-white text-lg font-medium font-roboto capitalize text-center w-full md:w-auto">Submit</button>
+          <div className="w-full flex justify-end ">
+            <button className="px-8 py-4 bg-gradient-custom rounded-lg max-w-[139px] h-[53px] text-white font-roboto font-medium text-[18px] leading-[100%] tracking-[0%] text-center capitalize  w-full ">Submit</button>
           </div>
         </div>
       </div>
